4253992bdd3b35725739383ab59c14e758235bcf
Vitaly Kurotkin <example@example.com>
2019-05-31 16:03:59 +0300

    Added files

--- app/src/main/java/com/kurotkin/cryptocurrencyapp/MainActivity.kt
+++ app/src/main/java/com/kurotkin/cryptocurrencyapp/MainActivity.kt
@@ -1,12 +1,31 @@
 package com.kurotkin.cryptocurrencyapp
 
+import android.content.res.Resources
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.LinearLayoutManager
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.schedulers.Schedulers
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val apiService = RetrofitGetInterface.create()
+        apiService.getCryptocurrency()
+            .observeOn(AndroidSchedulers.mainThread())  // Говорим в какой поток вернуть
+            .subscribeOn(Schedulers.io())               // Выбераем io - для работы с данными и интернетом
+            .subscribe({
+                    result -> arrayListInit(result)     // Здесь у нас калбек
+            }, { error ->
+                error.printStackTrace()
+            })
+
+        recyclerView.adapter = RecyclerViewAdapter(result, resources)
+        recyclerView.layoutManager = LinearLayoutManager(this)
+
     }
 }
---
+++ app/src/main/java/com/kurotkin/cryptocurrencyapp/RetrofitGetInterface.kt
@@ -0,0 +1,26 @@
+package com.kurotkin.cryptocurrencyapp
+
+import io.reactivex.Observable
+import retrofit2.Retrofit
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.GET
+
+interface RetrofitGetInterface {
+
+    @GET("v1/ticker/")
+    fun getCryptocurrency(): Observable<List<ResponseItem>>
+
+
+    companion object Factory {
+        fun create(): RetrofitGetInterface {
+            val retrofit = Retrofit.Builder()
+                .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+                .addConverterFactory(GsonConverterFactory.create())
+                .baseUrl("https://api.coinmarketcap.com/")
+                .build()
+
+            return retrofit.create(RetrofitGetInterface::class.java)
+        }
+    }
+}
