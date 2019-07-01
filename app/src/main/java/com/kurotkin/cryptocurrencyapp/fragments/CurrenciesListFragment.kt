940e0b0558bf73a9909da0ea8430e1f28d4c6ebb
Vitaly Kurotkin <example@example.com>
2019-07-01 16:47:19 +0300

    Подготовка к выводу списка

---
+++ app/src/main/java/com/kurotkin/cryptocurrencyapp/fragments/CurrenciesListFragment.kt
@@ -0,0 +1,17 @@
+package com.kurotkin.cryptocurrencyapp.fragments
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.kurotkin.cryptocurrencyapp.R
+
+class CurrenciesListFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? = inflater.inflate(R.layout.fragment_currencies_list, container, false)
+
+}
---
+++ app/src/main/java/com/kurotkin/cryptocurrencyapp/rest/CoinGeckoApi.kt
@@ -0,0 +1,24 @@
+package com.kurotkin.cryptocurrencyapp.rest
+
+import io.reactivex.Observable
+import retrofit2.http.GET
+import retrofit2.http.Path
+import retrofit2.http.Query
+
+interface CoinGeckoApi {
+
+    @GET("coins/markets")                                   //запрос списка криптовалют
+    fun getCoinMarket(
+        @Query("vs_currency") vs: String = "usd",
+        @Query("per_page") perPage: Int = 100,
+        @Query("sparkline") sparkline: Boolean = false,
+        @Query("order") order: String = "market_cap_desc"
+    ): Observable<List<GeckoCoin>>
+
+    @GET("coins/{id}/market_chart")                         //запрос данных для графика
+    fun getCoinMarketChart(
+        @Path("id") id: String,
+        @Query("vs_currency") vsCurrency: String = "usd",
+        @Query("days") days: String = "max"
+    ): Observable<GeckoCoinChart>
+}
